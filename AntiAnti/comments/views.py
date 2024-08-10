15c62bae087aad8cd9745cebc8e944a10f2b85ad
kalyuzhin <67906018+kalyuzhin@users.noreply.github.com>
2024-08-10 12:50:26 +0300

    Create views for home page

--- AntiAnti/comments/views.py
+++ AntiAnti/comments/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+from django.db.models import F
+from django.urls import reverse
 
-# Create your views here.
+from .forms import CommentForm
+from .models import Comment
+
+
+def home(request):
+    if request.method == 'POST':
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            comment_form.save()
+            print('KAIF')
+            return HttpResponseRedirect(reverse('home'))
+    comments = Comment.objects.all().order_by('-id')
+    context = {"comments": comments, 'form': CommentForm()}
+    return render(request, "index.html", context)
