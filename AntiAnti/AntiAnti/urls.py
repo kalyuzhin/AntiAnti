f196db9aa26a5a75eab35162ecac1aaa639801c5
kalyuzhin <67906018+kalyuzhin@users.noreply.github.com>
2024-08-10 13:02:13 +0300

    Quick fixes and make migrations

--- AntiAnti/AntiAnti/urls.py
+++ AntiAnti/AntiAnti/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    include('home/', include('comments.url')),
+    path('home/', include('comments.urls')),
     path('admin/', admin.site.urls),
 ]
