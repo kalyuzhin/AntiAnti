61bbebaaf840954be43e37e9dea7fa669888e27e
kalyuzhin <67906018+kalyuzhin@users.noreply.github.com>
2024-08-10 12:45:02 +0300

    Create Comment model class

--- AntiAnti/comments/models.py
+++ AntiAnti/comments/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Comment(models.Model):
+    input = models.TextField('')
+    # date = models.DateTimeField("date published")
+    date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.inputeld(auto_now_add=True)
